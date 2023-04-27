b07256f2134e992b87009b9d53a08a0bd856e022
tejas1j <43401993+tejas1j@users.noreply.github.com>
2023-04-27 19:14:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.llms import OpenAI
 import streamlit as st
 import os
 def main():
-    os.environ['OPENAI_API_KEY'] = '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'
+    os.environ['OPENAI_API_KEY'] = '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'
     st.title("PDF GPT creator")
     menu = ['Home','About']
     choice = st.sidebar.selectbox('Menu',menu)
